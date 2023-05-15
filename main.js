0893c12b2e73dfac90ab848068a3d2f92fce8d48
Vitor Ventury <95306019+Vitor-Ventury@users.noreply.github.com>
2023-05-14 21:04:47 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('form_id')
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const firstValue = document.getElementById('first_value');
+    const secondValue = document.getElementById('second_value');
+
+    firstValue.addEventListener('change', function(){
+        document.getElementById('btn_send').disabled = false
+    })
+    
+
+    if (secondValue.value > firstValue.value) {
+        document.getElementById('success_message').style.display='block';
+        document.getElementById('error_message').style.display='none';
+        document.getElementById('first_value').style.border=''
+        form.reset()
+    }
+    else {
+        document.getElementById('error_message').style.display='block'
+        document.getElementById('success_message').style.display='none'
+        document.getElementById('first_value').style.border='2px solid red'
+        document.getElementById('btn_send').disabled = true
+    }
+})
+
